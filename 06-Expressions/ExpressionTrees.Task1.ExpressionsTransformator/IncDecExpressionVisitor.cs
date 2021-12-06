0f57c661f65908b113e60c553c2ec0a4aabd4a08
Vilmantas Sukauskas <Vilmantas_Sukauskas@epam.com>
2021-12-06 09:03:47 +0200

    M6: initial commit

---
+++ 06-Expressions/ExpressionTrees.Task1.ExpressionsTransformator/IncDecExpressionVisitor.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq.Expressions;
+
+namespace ExpressionTrees.Task1.ExpressionsTransformer
+{
+    public class IncDecExpressionVisitor : ExpressionVisitor
+    {
+        // todo: feel free to add your code here
+    }
+}
---
+++ 06-Expressions/ExpressionTrees.Task1.ExpressionsTransformator/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Create a class based on ExpressionVisitor, which makes expression tree transformation:
+ * 1. converts expressions like <variable> + 1 to increment operations, <variable> - 1 - into decrement operations.
+ * 2. changes parameter values in a lambda expression to constants, taking the following as transformation parameters:
+ *    - source expression;
+ *    - dictionary: <parameter name: value for replacement>
+ * The results could be printed in console or checked via Debugger using any Visualizer.
+ */
+using System;
+
+namespace ExpressionTrees.Task1.ExpressionsTransformer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Expression Visitor for increment/decrement.");
+            Console.WriteLine();
+
+            // todo: feel free to add your code here
+
+            Console.ReadLine();
+        }
+    }
+}
